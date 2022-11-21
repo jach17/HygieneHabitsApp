68d9d10de5e6353af4f5f08957f8837443e035b0
Jonathan Dev <jonathan.aldana170500@gmail.com>
2022-11-21 16:54:16 -0600

    GRAFICA

--- app/src/main/java/com/devsolutions/hygienehabitsapp/Core/RetrofitHelper.kt
+++ app/src/main/java/com/devsolutions/hygienehabitsapp/Core/RetrofitHelper.kt
@@ -5,6 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
     val URL_BASE = "https://hygienehabitsback-production.up.railway.app/api/hygienehabits/"
+    val URL_BASE_LOCAL = "https://192.168.0.107:3000/api/hygienehabits/"
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(URL_BASE)
--- app/src/main/java/com/devsolutions/hygienehabitsapp/UI/App/DetallesReportes/DetailReportFragment.kt
+++ app/src/main/java/com/devsolutions/hygienehabitsapp/UI/App/DetallesReportes/DetailReportFragment.kt
@@ -1,5 +1,6 @@
 package com.devsolutions.hygienehabitsapp.UI.App.DetallesReportes
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,11 +12,18 @@ import com.devsolutions.hygienehabitsapp.Data.Model.Entities.ReporteModel
 import com.devsolutions.hygienehabitsapp.R
 import com.devsolutions.hygienehabitsapp.UI.App.HomeActivityViewModel
 import com.devsolutions.hygienehabitsapp.databinding.FragmentDetailReportBinding
+import com.echo.holographlibrary.PieGraph
+import com.echo.holographlibrary.PieSlice
+import kotlin.math.roundToInt
 
-class DetailReportFragment(reporteModel: ReporteModel) : DialogFragment() {
+class DetailReportFragment(val reporteModel: ReporteModel) : DialogFragment() {
 
     private lateinit var _binding: FragmentDetailReportBinding
     private val binding get() = _binding
+
+    private lateinit var pieGrafica: PieGraph
+
+    private var listaContent = ArrayList<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,15 +31,50 @@ class DetailReportFragment(reporteModel: ReporteModel) : DialogFragment() {
     ): View? {
         _binding = FragmentDetailReportBinding.inflate(layoutInflater, container, false)
 
-        binding.tvLevelNumber.text="LEVEL UNO XD"
+        initView()
 
         return binding.root
     }
+
+    fun graficar(list:ArrayList<String>){
+        for (item in list){
+            val rebanada = PieSlice()
+            rebanada.color = Color.parseColor(getRandomColor())
+            rebanada.value = 25f
+            pieGrafica.addSlice(rebanada)
+
+        }
+    }
+
+    private fun getRandomColor(): String {
+        val letras = arrayOf("0","1","2","3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
+        var color = "#"
+        for(i in 0 .. 5){
+            color+=letras[(Math.random()*15).roundToInt()]
+        }
+        return color
+    }
+
+    private fun initView() {
+
+        pieGrafica = binding.grafica
+
+        listaContent.add("Unomas")
+        listaContent.add("Unomas")
+        listaContent.add("Unomas")
+        listaContent.add("Unomas")
+
+        graficar(listaContent)
+    }
+
+
+
+
     override fun onResume() {
         super.onResume()
         val params: ViewGroup.LayoutParams = dialog!!.window!!.attributes
         params.width = WindowManager.LayoutParams.MATCH_PARENT
-        params.height = WindowManager.LayoutParams.MATCH_PARENT
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT
         dialog!!.window!!.attributes = params as WindowManager.LayoutParams
     }
 }
