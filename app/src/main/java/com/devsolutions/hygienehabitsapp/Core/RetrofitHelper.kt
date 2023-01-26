9cc3af1f46d6e14208783737d0535950bdb9d502
Jonathan Dev <jonathan.aldana170500@gmail.com>
2023-01-25 23:22:04 -0600

    Cambio para usar en local

--- app/src/main/java/com/devsolutions/hygienehabitsapp/Core/RetrofitHelper.kt
+++ app/src/main/java/com/devsolutions/hygienehabitsapp/Core/RetrofitHelper.kt
@@ -5,10 +5,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
     val URL_BASE = "https://hygienehabitsback-production.up.railway.app/api/hygienehabits/"
-    val URL_BASE_LOCAL = "https://192.168.0.107:3000/api/hygienehabits/"
+    val URL_BASE_LOCAL = "http://192.168.219.21:3000/api/hygienehabits/"
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(URL_BASE)
+            .baseUrl(URL_BASE_LOCAL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
